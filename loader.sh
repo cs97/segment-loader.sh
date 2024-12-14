89076353150f5b65bad53b0efce8dfd6205a26f1
cs97 <chrswr97@gmail.com>
2024-12-14 14:47:47 +0100

    Update loader.sh

--- loader.sh
+++ loader.sh
@@ -7,12 +7,18 @@ address=""
 
 if [ -d ./tmp ]; then
   mkdir tmp
+else
+  rm tmp/*
 fi
+
+download() {
+	wget -qO - ""$1"" > tmp/part$i.mp4 || download $1
+}
 
 for ((i=start;i<=range;i++))
 do
 	s=$(echo $address | sed s/"seg-1"/"seg-"$i/g)
 	echo "download $i of $range"
-	wget -qO - ""$s"" > tmp/part$i.mp4
+	#wget -qO - ""$s"" > tmp/part$i.mp4
+	download $s
 done
-
